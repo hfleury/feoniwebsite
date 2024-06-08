83d532445a22b6c4bf97ea1ebe508623a34bac99
henriqueoni <henrique@oniweb.com.br>
2024-06-08 12:25:48 +0200

    Add the Header component for tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header title='Oni Web Officer Henrique Fleury Cardoso 2'/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
